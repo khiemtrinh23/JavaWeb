3c4834b09ec5a6c0506987cc54dcc6133a92708e
khiemtrinh23 <khiemkhi2003@gmail.com>
2024-03-10 22:53:10 +0700

    Day11

---
+++ Day11/src/main/java/conn/Maintest.java
@@ -0,0 +1,11 @@
+package conn;
+
+import java.sql.SQLException;
+
+
+public class Maintest {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		System.out.println(TrinhVanKhiemConnection.getMSSQLConnection());
+	}
+}
